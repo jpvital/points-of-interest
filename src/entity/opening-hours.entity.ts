025b83662d0b43702cedf470d8051e6609afe609
João Campos <joao.campos18@gmail.com>
2024-05-17 14:01:02 +0100

    doc: opening-hours.entity
    Added comment

--- src/entity/opening-hours.entity.ts
+++ src/entity/opening-hours.entity.ts
@@ -1,6 +1,9 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { WeeklyHours } from "../types/schedule";
 
+// For a better understanding of this table, see the definition of WeeklyHours in src/types/schedule.ts.
+// With this implementation we have granularity on how to manage as many schedules as possible.
+
 @Entity()
 export class OpeningHours {
     @PrimaryGeneratedColumn('uuid');
